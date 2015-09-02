9665b54a9c1d333174d42d5bf256292a9181d421
charles <charles.kafels@hotmail.com>
2015-09-02 10:14:43 -0300

    - Implementing scenarios for unit test.

--- src/test/java/org/effting/tools/Desktop2Test.java
+++ src/test/java/org/effting/tools/Desktop2Test.java
@@ -6,6 +6,7 @@ import org.junit.runners.JUnit4;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
@@ -18,6 +19,7 @@ import static org.junit.Assert.assertEquals;
 public class Desktop2Test {
 
     private static final String USER_HOME = System.getProperty("user.home");
+    private static final String EDIT_FILE = "edit.txt";
 
     @Test
     public void should_be_linux_os() {
@@ -52,12 +54,43 @@ public class Desktop2Test {
     }
 
     @Test
-    public void should_open_file_manager() throws IOException {
-        final Operations operations = new UnixOperations();
-        operations.open(new File(USER_HOME));
+    public void should_be_unknown_os() {
+        System.setProperty("os.name", "Random Operating System");
 
+        final OperatingSystem os = OperatingSystem.current();
+        assertEquals(os, OperatingSystem.UNKNOWN);
+    }
+
+    @Test
+    public void should_show_user_home_directory() throws IOException {
+        // A fresh file manager should be opened.
+        Desktop2.getDesktop().open(new File(USER_HOME));
+
+        // Wait a few seconds to allow the process execution.
+        sleep(6);
+    }
+
+    @Test
+    public void should_open_browser() throws IOException {
+        // The default browser should be opened.
+        Desktop2.getDesktop().browser(URI.create("https://github.com/"));
+
+        // Wait a few seconds to allow the process execution.
+        sleep(6);
+    }
+
+    @Test
+    public void should_edit_file() throws IOException {
+        // The default editor text should be opened.
+        Desktop2.getDesktop().edit(new File(getClass().getResource(EDIT_FILE).getPath()));
+
+        // Wait a few seconds to allow the process execution.
+        sleep(6);
+    }
+
+    private void sleep(final int seconds) {
         try {
-            Thread.sleep(TimeUnit.MILLISECONDS.convert(6, TimeUnit.SECONDS));
+            Thread.sleep(TimeUnit.MILLISECONDS.convert(seconds, TimeUnit.SECONDS));
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
